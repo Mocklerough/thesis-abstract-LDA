0ea48bf81977581893696c40a803d7b4ff7fd6b8
Mocklerough <alexekarl@gmail.com>
2025-01-14 18:07:13 +0000

    Jan 14, 2025, 1:07 PM

--- abstract_LDA.py
+++ abstract_LDA.py
@@ -11,6 +11,8 @@ from gensim.corpora.dictionary import Dictionary
 import nltk
 nltk.download('punkt_tab')
 from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+import string
 
 with open('raw_article_data.pkl', 'rb') as handle:
     article_data = pickle.load(handle)    # list of {'title':str, 'abstract=str, doi_url=str, NODOI=int}, len=328
@@ -18,8 +20,21 @@ with open('raw_article_data.pkl', 'rb') as handle:
 # create a corpus of all article abstracts
 abstracts = [article['abstract_content'] for article in article_data]
 
-# TODO: remove punctuation and stopwords
+# tokenise to prepare the corpus
 tokenized_abstracts = [word_tokenize(abstract.lower()) for abstract in abstracts]
+# remove punctuation
+tokenized_abstracts = tokenized_abstracts.rstrip()
+tokenized_abstracts = re.sub(r'[^a-zA-Z]', ' ', tokenized_abstracts)
+# remove abstracts
+tokenized_abstracts = [" ".join([word for word in abstract if word not in stopwords.words('english') and not word.isdigit()]) for abstract in tokenized_abstracts]
+tokenized_abstracts = [''.join([word for word in abstract if word not in set(string.punctuation)]) for abstract in tokenized_abstracts]
+print(tokenized_abstracts[0])
 dictionary = Dictionary(tokenized_abstracts)
 corpus = [dictionary.doc2bow(abstract) for abstract in tokenized_abstracts]
-print(corpus)
+
+
+
+
+
+
+
