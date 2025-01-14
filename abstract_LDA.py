c6d6fb9e231c443b73843d5abfd2e0050e889347
Mocklerough <alexekarl@gmail.com>
2025-01-14 21:23:43 +0000

    Jan 14, 2025, 4:23 PM

--- abstract_LDA.py
+++ abstract_LDA.py
@@ -15,6 +15,7 @@ from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 import string
 import re
+from nltk.stem.wordnet import WordNetLemmatizer
 
 with open('raw_article_data.pkl', 'rb') as handle:
     article_data = pickle.load(handle)    # list of {'title':str, 'abstract=str, doi_url=str, NODOI=int}, len=328
@@ -26,6 +27,16 @@ abstracts = [article['abstract_content'] for article in article_data]
 abstracts = [re.sub(r'[^a-zA-Z]', ' ', abstract) for abstract in abstracts]
 tokenized_abstracts = [word_tokenize(abstract.lower()) for abstract in abstracts]
 tokenized_abstracts = [[word for word in abstract if word not in stopwords.words('english') and not word.isdigit()] for abstract in tokenized_abstracts]
+# lemmatize
+tokenized_abstracts = [" ".join(WordNetLemmatizer().lemmatize(word) for word in abstract.split()) for abstract in abstracts]
+# stem words
+
+for i in range(5):
+    print("Abstract ", i, ": ", tokenized_abstracts[i])
+
+
+
+
 
 dictionary = Dictionary(tokenized_abstracts)
 corpus = [dictionary.doc2bow(abstract) for abstract in tokenized_abstracts]
