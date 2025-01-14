b2aad0d778a60d989a8a668a5e2839b288a50ca6
Mocklerough <alexekarl@gmail.com>
2025-01-14 17:26:52 +0000

    Jan 14, 2025, 12:26 PM

--- abstract_LDA.py
+++ abstract_LDA.py
@@ -12,7 +12,8 @@ from nltk.tokenize import word_tokenize
 
 with open('raw_article_data.pkl', 'rb') as handle:
     article_data = pickle.load(handle)    # list of {'title':str, 'abstract=str, doi_url=str, NODOI=int}, len=328
-print(article_data[0])
-abstract_list = [article['abstract'] for article in article_data]
+
+# create a corpus of all article abstracts
+abstract_list = [article['abstract_content'] for article in article_data]
 for i in range(5):
     print(abstract_list[i])
